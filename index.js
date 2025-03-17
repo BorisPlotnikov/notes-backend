9c5949db9e49c10ad2f5670f1eb925acd6e47ce5
BorisPlotnikov <boriswo@gmail.com>
2025-03-16 20:07:06 -0700

    Fix database connection

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 
 // Import the Express library to set up the server
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const apiConfig = require('./utils/apiConfig');
 // Create an instance of the Express application
 const app = express();
 app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Connected to MongoDB"))
+.catch((error) => console.log("Error connecting to MongoDB: ", error));
 
 // Import the routes for handling '/notes' endpoint
 const notesRouter = require('./routes/notes');
--- utils/apiConfig.js
+++ utils/apiConfig.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 const apiConfig = {
-    apiUrl: process.env.API_URL,
-    port: process.env.PORT || 3000
+    // apiUrl: process.env.API_URL,
+    port: process.env.PORT || 3001
 };
 
 module.exports = apiConfig;
