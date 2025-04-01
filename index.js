21eec6783fa88de954c0aae831aedc449c5aeb82
BorisPlotnikov <boriswo@gmail.com>
2025-03-31 17:41:00 -0700

    Removed all comments, Resolves #48

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // index.js
 
-// Import the Express library to set up the server
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const apiConfig = require('./utils/apiConfig');
-// Create an instance of the Express application
 const app = express();
 app.use(cors());
 
@@ -15,25 +13,17 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log("Connected to MongoDB"))
 .catch((error) => console.log("Error connecting to MongoDB: ", error));
 
-// Import the routes for handling '/notes' endpoint
 const notesRouter = require('./routes/notes');
-// Import the custom error handler middleware
 const errorHandler = require('./middleware/errorHandler');
 
-// Middleware to parse incoming JSON request bodies
 app.use(express.json());
 
-// Set up the '/notes' route to use the notesRouter defined in 'routes/notes.js'
 app.use('/notes', notesRouter);
 
-// Use the error handler middleware to catch and handle errors
 app.use(errorHandler);
 
-// Define the port the server will listen on, using the environment variable or default to 3000
 const PORT = apiConfig.port;
 
-// Start the Express server and listen on the specified port
 app.listen(PORT, () => {
-    // Log a message indicating that the server is running and the port it's listening on
     console.log(`Server running on port ${PORT}`);
 });
--- utils/apiConfig.js
+++ utils/apiConfig.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const apiConfig = {
-    // apiUrl: process.env.API_URL,
     port: process.env.PORT || 3001
 };
 
