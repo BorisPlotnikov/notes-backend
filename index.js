5dd14280b1eeb17376fb286c97eea73b09739c6f
BorisPlotnikov <boriswo@gmail.com>
2025-04-07 17:18:02 -0700

    Separating HTTP methods into separate files. Created blank files

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 // index.js
 
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const apiConfig = require('./utils/apiConfig');
 const app = express();
+app.use(express.json());
+
+const cors = require('cors');
 app.use(cors());
 
+const errorHandler = require('./middleware/errorHandler');
+const apiConfig = require('./utils/apiConfig');
+app.use(errorHandler);
+const notesRouter = require('./routes/notes');
+app.use('/notes', notesRouter);
+
+const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("Connected to MongoDB"))
 .catch((error) => console.log("Error connecting to MongoDB: ", error));
 
-const notesRouter = require('./routes/notes');
-const errorHandler = require('./middleware/errorHandler');
-
-app.use(express.json());
-
-app.use('/notes', notesRouter);
-
-app.use(errorHandler);
-
 const PORT = apiConfig.port;
-
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
